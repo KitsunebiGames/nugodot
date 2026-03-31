66f6f101f72d20f8c3518200e900d2df4d5f88c1
LunaTheFoxgirl <luna@foxgirls.gay>
2026-03-31 13:38:58 +0200

    Fix enum binding

--- source/godot/core/bind.d
+++ source/godot/core/bind.d
@@ -297,10 +297,12 @@ void gde_bind_const(T, alias memberName)() @nogc {
     } else {
 
         // Manifest constants and consts
+        p_enumname = gde_make_string_name(__traits(identifier, member));
         p_constname = gde_make_string_name(toScreamingSnakeCase!(__traits(identifier, member)));
         p_value = cast(GDExtensionInt)__traits(getMember, T, memberName);
         classdb_register_extension_class_integer_constant(__godot_class_library, p_classname, p_enumname, p_constname, p_value, false);
         gde_free_string_name(p_constname);
+        gde_free_string_name(p_enumname);
     }
 
     gde_free_string_name(p_classname);
